0beb26b014ef802be911cb275c58ac141424fde3
Vincenzo Marcovecchio <vinceoldmark@gmail.com>
2020-08-07 22:41:45 +0200

    Updates

---
+++ precache-manifest.d8fc8d73f20a574465506edcaa3f3b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b31f42ae02017a65aca1edf9152fec",
+    "url": "/Spotify-clone/index.html"
+  },
+  {
+    "revision": "e78a7dc972b8d3acf996",
+    "url": "/Spotify-clone/static/css/main.74606462.chunk.css"
+  },
+  {
+    "revision": "7335ade03470aa339b23",
+    "url": "/Spotify-clone/static/js/2.f883416f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Spotify-clone/static/js/2.f883416f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78a7dc972b8d3acf996",
+    "url": "/Spotify-clone/static/js/main.120b4209.chunk.js"
+  },
+  {
+    "revision": "f96f1814e8713817bc32",
+    "url": "/Spotify-clone/static/js/runtime-main.a89679a3.js"
+  }
+]);
